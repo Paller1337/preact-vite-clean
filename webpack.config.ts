07f6fc3a68050b02c370f880b8c9c29c98936349
Paller1337 <max.paller@yandex.ru>
2023-11-10 19:36:41 +0300

    first commit

---
+++ webpack.config.ts
@@ -0,0 +1,23 @@
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.(scss|sass)$/,
+                use: [
+                    'style-loader', // creates style nodes from JS strings
+                    'css-loader',   // translates CSS into CommonJS
+                    'sass-loader'   // compiles Sass to CSS
+                ],
+            },
+        ],
+    },
+    resolve: {
+        alias: {
+            'react': 'preact/compat',
+            'react-dom/test-utils': 'preact/test-utils',
+            'react-dom': 'preact/compat',
+            'react/jsx-runtime': 'preact/jsx-runtime'
+        }
+    }
+};
+``
